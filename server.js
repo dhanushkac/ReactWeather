3409355933ceca947cec8e0a6c34b3071ba4dae0
Dhanushka C <dhanushkac@thinkcube.com>
2016-11-11 15:04:47 +0530

    few changes added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 //create our app
 var app = express();
+
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
@@ -15,5 +16,5 @@ app.use(function (req, res, next) {
 app.use(express.static('public'));
 
 app.listen(PORT, function(){
-	console.log('Express server is up on port %s', 3000);
+	console.log('Express server is up on port %s', PORT);
 });
